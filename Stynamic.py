094d4766665a17f2ecfa8f691c002efad666e099
anthony <caffeineinfused@gmail.com>
2017-04-09 12:34:02 -0700

    adding help text to command line flags

--- Stynamic.py
+++ Stynamic.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python
 import argparse
+import sys
 
 class Stynamic():
-	
-
+	flags = []
 	def parseOpts(self):
 		parser = argparse.ArgumentParser(description = "Stynamic")
 		group0 = parser.add_mutually_exclusive_group()
-		group0.add_argument('-v', action='store_true')
-		group0.add_argument('-q', action='store_true')
-		group0.add_argument('-d', action='store_true')
+		group0.add_argument('-v', action='store_true', help='Select verbose output')
+		group0.add_argument('-q', action='store_true', help='Select quiet output')
+		group0.add_argument('-d', action='store_true', help='Select default output')
 
 		group1 = parser.add_argument_group()
-		group1.add_argument('-m', metavar='makefile', action='store')
-		group1.add_argument('-b', metavar='binary', action='store')
+		group1.add_argument('-m', metavar='makefile', action='store', help='Specify makefile location to augment for running with Stynamic')
+		group1.add_argument('-b', metavar='binary', action='store', help='Specify binary location for running with Stynamic')
 
 		group2 = parser.add_mutually_exclusive_group()
-		group2.add_argument('-a', action='store_true', required=False)
-		group2.add_argument('-f', metavar='files', nargs='+', action='append',required=False)
-		parser.parse_known_args()
-
-	#def instValgWrapper():
-
-	#def setQuickFlags():
-
-	#def setVerboseFlags():
-
+		group2.add_argument('-a', action='store_true', required=False, help='Have Stynamic automatically determine source file list')
+		group2.add_argument('-f', metavar='file', nargs='+', action='append',required=False,help='Specify files for Stynamic to check')
+		flags = parser.parse_known_args(sys.argv[1:])
+		print flags
+	def instValgWrapper():
+		#vl = ValWrap()
+		#vl.setProg('./testFiles/ValTester')
+		#vl.setArgs
 def main():
 	Styn = Stynamic()
 	Styn.parseOpts()
+	#Styn.instValgWrapper()
 
 if __name__ == '__main__': main()
