5f17ee60e2670873f0e407a19678fdf9141100eb
Cory Linfield <c1field@gmail.com>
2017-04-09 13:35:30 -0600

    Working on Stynamic.py flawfinder wrapper interaction

--- Stynamic.py
+++ Stynamic.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import argparse
+import FlawFndr
 
 class Stynamic():
 	
@@ -7,17 +8,17 @@ class Stynamic():
 	def parseOpts(self):
 		parser = argparse.ArgumentParser(description = "Stynamic")
 		group0 = parser.add_mutually_exclusive_group()
-		group0.add_argument('-v', action='store_true')
-		group0.add_argument('-q', action='store_true')
+		group0.add_argument('-v', metavar='verbose', action='store_true', help='verbose output, exclusive from -q')
+		group0.add_argument('-q', metavar='quick', action='store_true', help='quick output, exclusive from -v')
 		group0.add_argument('-d', action='store_true')
 
 		group1 = parser.add_argument_group()
-		group1.add_argument('-m', metavar='makefile', action='store')
-		group1.add_argument('-b', metavar='binary', action='store')
+		group1.add_argument('-m', metavar='makefile', action='store', help='path to makefile')
+		group1.add_argument('-b', metavar='binary', action='store', help='path to binary')
 
 		group2 = parser.add_mutually_exclusive_group()
-		group2.add_argument('-a', action='store_true', required=False)
-		group2.add_argument('-f', metavar='files', nargs='+', action='append',required=False)
+		group2.add_argument('-a', action='store_true', help='auto file list, exclusive from -f, proposes file list', required=False)
+		group2.add_argument('-f', metavar='files', nargs='+', action='append', help='file list (can be taken multiple times)', required=False)
 		parser.parse_known_args()
 
 	#def instValgWrapper():
@@ -26,8 +27,30 @@ class Stynamic():
 
 	#def setVerboseFlags():
 
+	def setFFVerboseFlags(self):
+		return '-n -m 0 --followdotdir'
+
+	def setFFQuickFlags(self):
+		return '-F -m 4'
+
+	def setFFDefaultFlags(self):
+		return ''
+
+	def instFlawfWrapper(self):
+		ffargs = '-c'
+
+		if (verbose):
+			ffargs += setFFVerboseFlags()
+		elif (quick):
+			ffargs += setFFQuickFlags()
+		else:
+
+		fw = FlawFndr.FlawFinder()
+
+
 def main():
 	Styn = Stynamic()
 	Styn.parseOpts()
+	Styn.instFlawfWrapper()
 
 if __name__ == '__main__': main()
